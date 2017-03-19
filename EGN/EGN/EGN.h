9bb0e23cfff79439de732c83fce3fea551fdb4fb
Gratsiela <gratsiela.gancheva@gmail.com>
2017-03-19 15:46:51 +0200

    19/03
    Class EGN

---
+++ EGN/EGN/EGN.h
@@ -0,0 +1,42 @@
+﻿#ifndef EGN_H_INCLUDE
+#define EGN_H_INCLUDE
+
+class EGN
+{
+private:
+	char allNum[10];
+	int year;    
+	int month;    
+	int day;    
+	int region;  
+	char gender;
+	int chekNum;  
+	///
+	///check if the EGN is valid
+	///and set the value of all integer variables
+	///
+	const bool isValid(const char * egn) const;
+	void set_YEAR_MONTH_DAY(const char * egn);
+	void set_REGION(const char * egn);
+	void set_GENDER(const char * egn);
+	void set_CHECK_NUM(const char * egn);
+	
+public:
+	EGN();
+	EGN(const char * egn);
+	~EGN(); //TODO ??
+
+	//char* getEGN() const;  //TODO ??
+	int getYear()const;
+	int getMonth()const;
+	int getDay() const;
+	char getGender() const;
+
+	const void printRegion() const;
+	const void printBirthday() const;
+	const void printGender() const;
+	
+	void setEGN(const char* egn);
+};
+
+#endif //EGN_H_INCLUDE
---
+++ EGN/EGN/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include"EGN.h"
+
+int main() 
+{
+	char egntest[11];
+	EGN test;
+	std::cout << "INSERT EGN: ";
+	std::cin.clear();
+	std::cin.getline(egntest, 11);
+	//std::cin.getline(test.setEGN, 11); //How to use & to create pointer to member ?
+	test.setEGN(egntest);
+
+	std::cout << "Your birthday is: "; test.printBirthday();
+	std::cout << "Your region is: "; test.printRegion();
+	
+	std::cout 
+		<< "Your gender is: " << test.getGender() << std::endl
+		<< "Your day is: "<< test.getDay() << std::endl
+		<< "Your mounth is: " << test.getMonth() << std::endl
+		<< "Your year is: " << test.getYear() << std::endl;
+	
+	std::cout << "Your gender is: "; test.printGender();
+
+	return 0;
+}
